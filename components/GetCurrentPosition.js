75f669636b5150344f9ffe0c1825189c1e0d9e25
Henri Bergius <henri.bergius@iki.fi>
2018-11-03 18:03:32 +0100

    Add component for getting current geolocation

---
+++ components/GetCurrentPosition.js
@@ -0,0 +1,36 @@
+const noflo = require('noflo');
+
+// @runtime noflo-browser
+
+exports.getComponent = () => {
+  const c = new noflo.Component();
+  c.description = 'Get user\'s current location';
+  c.icon = 'location-arrow';
+  c.inPorts.add('in', {
+    datatype: 'bang',
+  });
+  c.outPorts.add('out', {
+    datatype: 'object',
+  });
+  c.outPorts.add('error', {
+    datatype: 'object',
+  });
+  c.process((input, output) => {
+    if (!input.hasData('in')) {
+      return;
+    }
+    input.getData('in');
+    if (!navigator.geolocation) {
+      output.done(new Error('Location API not available'));
+      return;
+    }
+    navigator.geolocation.getCurrentPosition((pos) => {
+      output.sendDone({
+        out: {
+          ...pos.coords,
+        },
+      });
+    }, output.done);
+  });
+  return c;
+};
